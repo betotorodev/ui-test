4260825fc2f220fa901a9519b4f94ee426892fec
Beto <betotoro0902@gmail.com>
2021-09-14 18:34:40 -0500

    [STYLE]: finish desktop list card layout

--- src/components/ListOfCards/components/Card/index.js
+++ src/components/ListOfCards/components/Card/index.js
@@ -10,15 +10,41 @@ export const Card = () => {
       <figure className='card__image-container'>
         <img src='https://i.imgur.com/bbptQSV.png' alt='Mark Zuckerberg' />
       </figure>
+      <aside className='card__information'>
+        <section className='card__information-profile'>
+          <button className='card__button card__thumbUp'>
+            <ThumbUp width='22' height='22' />
+          </button>
+          <div>
+            <h1>Mark Zuckerberg</h1>
+            <p>
+              Vestibulum diam ante, porttitor a odio eget, rhoncus neque. Aenean
+              eu velit…
+            </p>
+          </div>
+        </section>
+        <section className='card__information-poll'>
+          <p>1 month ago in Entertainment</p>
+          <div className='card__buttons-container'>
+            <button className='card__button card__thumbUp'>
+              <ThumbUp width='22' height='22' />
+            </button>
+            <button className='card__button card__thumbDown'>
+              <ThumbDown width='22' height='22' />
+            </button>
+            <button className='card__button--vote'>Vote now</button>
+          </div>
+        </section>
+      </aside>
       <footer className='card__poll'>
-        <div className='poll card__thumbUp' style={{ width: width + '%' }}>
+        <button className='poll card__thumbUp' style={{ width: width + '%' }}>
           <ThumbUp width='22' height='22' />
           <span>{width}%</span>
-        </div>
-        <div className='poll card__thumbDown' style={{ width: width + '%' }}>
+        </button>
+        <button className='poll card__thumbDown' style={{ width: width + '%' }}>
           <ThumbDown width='22' height='22' />
           <span>{width}%</span>
-        </div>
+        </button>
       </footer>
     </div>
   )
