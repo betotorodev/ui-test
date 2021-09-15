5805126562498105b770101b62bbd2b23b61a5ce
Beto <betotoro0902@gmail.com>
2021-09-15 14:48:21 -0500

    [STYLE]: adjust mobile layout

--- src/components/ListOfCards/index.js
+++ src/components/ListOfCards/index.js
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Card } from './components/Card'
 import './styles.css'
 
 export const ListOfCards = () => {
-  const [listView, setListView] = useState('List')
+  const [listView, setListView] = useState('Grid')
+  const [screenSize, setScreenSize] = useState(0)
   const isGrid = listView === 'Grid'
   const array = Array(6).fill('')
+
+  useEffect(() => {
+    let windowSize = window.screen.width
+    setScreenSize(windowSize)
+    const listener = window.addEventListener('resize', (e) => {
+      windowSize = e.currentTarget.innerWidth
+      setScreenSize(windowSize)
+      if (windowSize <= 500) {
+        setListView('Grid')
+      }
+      console.log(e.currentTarget.innerWidth)
+    })
+
+    return () => {
+      window.removeEventListener('resize', listener)
+    }
+  }, [])
+
   const handleChange = (e) => {
     const { value } = e.target
     setListView(value)
@@ -15,10 +34,12 @@ export const ListOfCards = () => {
     <main role='main'>
       <section className='poll__title'>
         <h1>Previus Rulings</h1>
-        <select value={listView} onChange={handleChange} name='Type of view'>
-          <option value='List'>List</option>
-          <option value='Grid'>Grid</option>
-        </select>
+        {screenSize > 500 && (
+          <select value={listView} onChange={handleChange} name='Type of view'>
+            <option value='List'>List</option>
+            <option value='Grid'>Grid</option>
+          </select>
+        )}
       </section>
       <section className={`${isGrid ? 'grid__layout' : 'list__layout'}`}>
         {array.map((item, index) => (
